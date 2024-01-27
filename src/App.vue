4de4baa8a8bf894a38934e85128914c3b7503537
dol <dol@dols-MacBook-Air.local>
2024-01-27 18:27:47 +0700

    [Create] Create based varibles, and temp functions for user logs, and scores

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup>
+
+import { ref } from 'vue';
+
+const user_event_log = ref([]) //[{x: x, x: x}]
+const score_counting = ref([]) //[{round: x, score: x}]
+
+const push_log = (user_event) => { user_event_log.push(user_event) }
+const push_score = (user_score) => { score_counting.push(user_score) }
+
 </script>
 
 <template>
-  <h1>Simon Says</h1>
+	<h1>Simon Says</h1>
 </template>
 
 <style scoped>
