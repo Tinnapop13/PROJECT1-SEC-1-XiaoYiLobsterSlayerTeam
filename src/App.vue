d384937d605b6c9c3c72f142fd7c2884f2480454
Natsaran Sae-oueng <zhinobid@gmail.com>
2024-02-08 10:14:01 +0700

    [Fixed] Change Toggle Theme code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
 import { ref, reactive } from "vue" 
-import { useDark, useToggle } from "@vueuse/core";
 
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
+const isDark = ref(false)
+const toggleDark = () => { isDark.value = !isDark.value}
 
 // [papangkorn00] Homepage UI Variable , Handle Malfunction Player Input ,  Loading Variable
 const showHomePage = ref(true)
 const showGamePage = ref(false)
 const showPopupTutorial = ref(false)
-const showProgressBar = ref(false)
-const i = ref(0)
-const progressBarWidth = ref(0)
 let disableStart = false
 let disablePlay = true
+let disableReset = true
 
 
 // [phatcharadol] Game Result Variable , Game Result UI Variable , Timer Variable
@@ -21,8 +18,7 @@ const showPopupEnd = ref(false)
 const firstUint = ref(59)
 const secondUint = ref(9)
 const playerLog = ref([{round:0}])
-const round = ref(1)
-
+const round = ref(0)
 
 // [Tinnapop13] Show Trace Variable 
 let gameRoundPointer = 0
@@ -34,13 +30,12 @@ const traces = []
 const logLst = []
 let logIndex = 0
 
-
 // Game Properties Variable
 const buttons = reactive([
-  { number: 0, color: "bg-[#FF0000]" },
-  { number: 1, color: "bg-[#228B22]" },
-  { number: 2, color: "bg-[#0000FF]" },
-  { number: 3, color: "bg-[#FFFF00]" },
+  {number: 0, color: "bg-[#FF0000]"},
+  {number: 1, color: "bg-[#228B22]"},
+  {number: 2, color: "bg-[#0000FF]"},
+  {number: 3, color: "bg-[#FFFF00]"},
 ])
 
 
@@ -50,42 +45,24 @@ const togglePopupTutorial = () => {
 }
 
 const startToggle = () => {
-  setTimeout(() => {
-    showGamePage.value = true
-  }, 1000)
+  showGamePage.value = true
   showHomePage.value = false
-  showProgressBar.value = true
-  move()
-  progressBarShow()
 }
 
-const progressBarShow = () => {
-  setTimeout(() => {
-    showProgressBar.value = false
-  }, 1000)
+const resetGame = () => {
+  playerLog.value = [{ round: 0 }]
+  gameRoundPointer = 0
+  traceButtonIndex.value = -1
+  traces.splice(0, traces.length)
+  logLst.splice(0, logLst.length)
+  logIndex = 0
+  disableStart = false
+  togglePopupEnd()
 }
-
-const move = () => {
-  if (i.value === 0) {
-    i.value = 1
-    let width = 1
-    const id = setInterval(() => {
-      if (width >= 100) {
-        clearInterval(id)
-        i.value = 0
-      } else {
-        width++
-        progressBarWidth.value = `${width}%`
-      }
-    }, 10)
-  }
-}
-
 
 // [Tinnapop13] Display Trace function
 const randomNumber = (max) => {
-  return Math.floor(Math.random
-  () * max)
+  return Math.floor(Math.random() * max)
 }
 
 const showTraceState = (buttonNumber) => {
@@ -99,8 +76,10 @@ const displayTrace = () => {
   // Block Player From Clicking
   disableStart = true
   disablePlay = true
+  disableReset = false
   // Start Timer While Game Start Once
-  if (round.value === 1) {
+  if (round.value === 0) {
+    round.value++
     playerTimer()
   }
   // Set Button to Change Style while Show Trace
@@ -138,7 +117,8 @@ const playerClick = (event) => {
   setTimeout(() => {
     traceButtonIndex.value = -1
   }, 100)
-  if (logLst[logIndex] === traces[logIndex]) {
+  if (logLst[logIndex] === traces[logIndex]) {    
+    new Audio("https://cdn.discordapp.com/attachments/1196805209381404682/1203715260742238268/pop-cat-original-meme.mp3?ex=65d21a32&is=65bfa532&hm=011af7cfcb702bb425a2ca043e29decd78bcb3b7ce4751ed44d573dffd27ad4a&").play()
     logIndex++
     if (logLst.length === traces.length) {
       logLst.splice(0, logLst.length) // reset Array
@@ -146,77 +126,66 @@ const playerClick = (event) => {
       round.value++
       displayTrace()
     }
-  }
-  else {
-    console.log('lose')
+  } else {
+    console.log("lose")
     logLst.splice(0, logLst.length) // reset Array
     traces.splice(0, traces.length) // reset Array
     logIndex = 0
     disableStart = false
-    showPopupEnd.value = true
+    disablePlay = true
+    showPopupEnd.value = true //ntf
+    disableReset = true
+
+    
   }
 }
 
-
-// [phatcharadol] Game Result function , Timer function , Calculate Score Level 
+// [phatcharadol] Game Result function , Timer function , Calculate Score Level
 const togglePopupEnd = () => {
   showPopupEnd.value = !showPopupEnd.value
-  round.value = 1
+  round.value = 0
   firstUint.value = 59
   secondUint.value = 9
+  disableReset = true
 }
 
 const playerTimer = () => {
   const timer = setInterval(() => {
     firstUint.value--
-
     if (firstUint.value < 10) firstUint.value = `0${firstUint.value}`
 
     if (firstUint.value == 0) {
       secondUint.value--
-      secondUint.value === 0 ? firstUint.value = 0 : firstUint.value = 59
-    }
-
-    if (showPopupEnd.value || secondUint.value <= 0) {
-      clearInterval(timer)
-      playerLog.value.push({
-        time: `${secondUint.value}:${firstUint.value}`,
-        round: round.value-1,
-      })
-      showPopupEnd.value = true
+      secondUint.value === 0 ? (firstUint.value = 0) : (firstUint.value = 59)
     }
   }, 1000)
+
+  const checkTimer = setInterval(()=>{
+    if (showPopupEnd.value || secondUint.value <= 0) {
+      clearInterval(timer)
+      clearInterval(checkTimer)
+      playerLog.value.push({
+        time: `${secondUint.value}:${firstUint.value}`,
+        round: round.value,
+      })
+      new Audio("https://cdn.discordapp.com/attachments/1196805209381404682/1203710805413986344/rock-eyebrow-raise-sound-effect.mp3?ex=65d2160b&is=65bfa10b&hm=289ed8577d1a08479c2cd3f9607a37cb48240dac25744204c1ab4181e91992c1&").play()
+    }
+  },100)
 }
-
-const calculateScore = () => {
-  // Temp....
-
-  // if (playerLog.score < 5 && timeCounter.value >= 10) {
-  // } else if (playerLog.score < 15 && timeCounter.value >= 20) {
-  // } else if (playerLog.score < 20 && timeCounter.value >= 30) {
-  // }
-
-  // if (playerLog.score < 5 && timeCounter.value >= 10) {
-
-  // } else if (playerLog.score < 15 && timeCounter.value >= 20) 
-
-  // } else if (playerLog.score < 20 && timeCounter.value >= 30) 
-}
-
-
 
 
 </script>
 
 <template>
   <!-- Homepage -->
-  <section v-if="showHomePage" class="flex flex-col h-screen dark:bg-[#121212]">
+
+  <section v-if="showHomePage" class="flex flex-col h-screen" :class = "isDark ? 'bg-[#121212]': ''">
     <div class="flex justify-end margin mt-3 mr-5" v-if="isDark"><input type="checkbox" class="toggle" checked @click="toggleDark()" /></div>
     <div class="flex justify-end margin mt-3 mr-5" v-else><input type="checkbox" class="toggle"  @click="toggleDark()" /></div>
     <div class="max-w-screen-lg mx-auto my-4 flex flex-col gap-20 items-center justify-center h-screen px-4 md:flex-row">
       <div class="flex flex-col justify-center">
-        <h1 class="text-2xl sm:text-7xl font-bold dark:text-white">Simon Says</h1>
-        <p class="text-gray-400 py-4 max-w-md dark:text-white">
+        <h1 class="text-2xl sm:text-7xl font-bold" :class = "isDark ? 'text-white': ''" >Simon Says</h1>
+        <p class="text-gray-400 py-4 max-w-md" :class = "isDark ? 'text-white': ''">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quos
           et vel recusandae illum, voluptates iusto deleniti dolorem obcaecati
           similique optio adipisci assumenda dignissimos, quo quisquam eaque
@@ -233,7 +202,7 @@ const calculateScore = () => {
 
     <div class="flex justify-center">
       <button @click="startToggle"
-        class="btn btn-primary  rounded-2xl w-64 text-white transition duration-300 ease-in-out">
+        class="btn btn-primary rounded-2xl w-64 text-white transition duration-300 ease-in-out">
         PLAY
       </button>
     </div>
@@ -246,9 +215,10 @@ const calculateScore = () => {
   </section>
 
 
+
   <!-- Tutorial pop-up -->
-  <section v-if="showPopupTutorial" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 dark:bg-[#121212]">
-    <div class="w-full sm:w-96 lg:w-1/2 text-center bg-white p-8 rounded-lg dark:bg-[#121212]">
+  <section v-if="showPopupTutorial" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 " >
+    <div class="w-full sm:w-96 lg:w-1/2 text-center bg-white p-8 rounded-lg ">
       <h1 class="text-2xl font-bold mb-4">This is a tutorial</h1>
 
       <div class="mb-4 ">
@@ -264,24 +234,17 @@ const calculateScore = () => {
     </div>
   </section>
 
-  <!--Loading progress bar -->
-  <section v-if="showProgressBar" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 dark:bg-[#121212]">
-    <div class="w-64 bg-gray-300 rounded-md">
-      <div class="w-[1%] h-8 bg-gradient-to-r from-red-500 from-10% via-green-500 via-30% to-blue-500 to-90% rounded-md"
-        :style="{ width: progressBarWidth }"></div>
-    </div>
-  </section>
 
   <!-- Gamepage UI -->
-    <section v-if="showGamePage" class="min-h-screen flex flex-col items-center justify-center dark:bg-[#121212]">
+  <Transition name="scale">
+    <section v-if="showGamePage" class="min-h-screen flex flex-col items-center justify-center" :class = "isDark ? 'bg-[#121212]': ''">
       <div class="flex justify-end margin mt-3 mr-5" v-if="isDark"><input type="checkbox" class="toggle" checked @click="toggleDark()" /></div>
       <div class="flex justify-end margin mt-3 mr-5" v-else><input type="checkbox" class="toggle"  @click="toggleDark()" /></div>
-      <h1 class="font-bold text-5xl my-10 dark:text-white">Simon Says</h1>
+      <h1 class="font-bold text-5xl my-10" :class = "isDark ? 'text-white': ''">Simon Says</h1>
       <div class="flex justify-between w-96 my-4 max-sm:w-80" >
-        <span class="countdown font-mono text-2xl dark:text-white">Round : {{ round }}</span>
-        <span class="countdown font-mono text-2xl dark:text-white">{{ secondUint }} : {{ firstUint }}</span>
+        <span class="countdown font-mono text-2xl" :class = "isDark ? 'text-white': ''">Round : {{ round }}</span>
+        <span class="countdown font-mono text-2xl" :class = "isDark ? 'text-white': ''">{{ secondUint }} : {{ firstUint }}</span>
       </div>
-      
       <main class="grid grid-cols-2 gap-x-5 gap-y-5 w-96 sm:h-96 max-sm:w-80">
         <button v-for="buttonNumber in buttons" :class="showTraceState(buttonNumber.number)" :disabled="disablePlay"
           :key="buttonNumber" :id="buttonNumber.number" class="rounded-md h-44 hover:brightness-90 active:brightness-150"
@@ -290,16 +253,32 @@ const calculateScore = () => {
       <button class="btn btn-primary m-10 max-sm:size- w-96 max-sm:w-80 text-white" @click="displayTrace" :disabled="disableStart">
         START
       </button>
+      <button class="btn btn-primary text-white" @click="resetGame" :disabled="disableReset">
+        Reset
+      </button>
+      <button class="btn btn-primary w-96 max-sm:w-80 text-white" @click="showGamePage = false; showHomePage = true"
+        :disabled="disableStart">
+        HOME
+      </button>
     </section>
-
-
+  </Transition>
   <section v-if="showPopupEnd" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
     <div class="w-80 text-center bg-white p-8 rounded-lg">
       <h1 class="text-2xl font-bold mb-4">End!</h1>
       <div>
-        Round: {{ playerLog[playerLog.length - 1].round }}
+        <div v-if="playerLog[playerLog.length - 1].round <= 10">
+          TRY AGAIN NOOB!
+        </div>
+        <div v-else-if="playerLog[playerLog.length - 1].round > 10 && playerLog[playerLog.length - 1].round < 30">
+          THAT KINDA DECENT...
+        </div>
+        <div v-else>
+          EXCELLENT!
+        </div>
         <br>
-        Time: {{ playerLog[playerLog.length - 1].time }}
+        Finished Round: {{ playerLog[playerLog.length - 1].round }}
+        <br>
+        Finished Time: {{ playerLog[playerLog.length - 1].time }}
       </div>
 
       <button @click="togglePopupEnd" class="btn btn-warning text-white px-4 py-2 mt-4">
@@ -309,4 +288,18 @@ const calculateScore = () => {
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.scale-enter-active {
+  animation: scale 1s;
+}
+
+@keyframes scale {
+  0% {
+    transform: scale(0);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
